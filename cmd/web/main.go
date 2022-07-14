3bc34e438a58a02b26801ca77c3cd8c380b25d2a
Marcio Silva <marcionps@proton.me>
2022-07-14 18:02:21 -0300

    added reservation summary template and handler

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/marcionps/bookings/internal/config"
 	"github.com/marcionps/bookings/internal/handlers"
+	"github.com/marcionps/bookings/internal/models"
 	"github.com/marcionps/bookings/internal/render"
 )
 
@@ -18,6 +20,9 @@ var app config.AppConfig
 var session *scs.SessionManager
 
 func main() {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
 	// change this to true when in production
 	app.InProduction = false
 
