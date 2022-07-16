6b3f444a26c64636211b228b4185e67b7024b8c4
Marcio Silva <marcionps@proton.me>
2022-07-16 20:54:15 -0300

    added error logs and helpers functions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/gob"
 	"fmt"
+	"github.com/marcionps/bookings/internal/helpers"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
@@ -18,6 +20,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 func main() {
 	err := run()
@@ -43,6 +47,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -64,6 +74,8 @@ func run() error {
 	handlers.NewHandlers(repo)
 
 	render.NewTemplate(&app)
+
+	helpers.NewHelpers(&app)
 
 	return nil
 }
