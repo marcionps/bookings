3dc34a22a543c1bae5be19b952031b6c31a60d24
Marcio Silva <marcionps@proton.me>
2022-07-19 15:27:58 -0300

    added insert reservation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -8,6 +8,8 @@ import (
 	"github.com/marcionps/bookings/internal/repository"
 	"github.com/marcionps/bookings/internal/repository/dbrepo"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/marcionps/bookings/internal/config"
 	"github.com/marcionps/bookings/internal/forms"
@@ -67,11 +69,34 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// 01/02 03:04:05PM '06 -0700
+	sd := r.Form.Get("start_date")
+	ed := r.Form.Get("end_date")
+
+	layout := "2006-01-02"
+
+	startDate, err := time.Parse(layout, sd)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+	endDate, err := time.Parse(layout, ed)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
+	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
 	reservation := models.Reservation{
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
 		Email:     r.Form.Get("email"),
 		Phone:     r.Form.Get("phone"),
+		StartDate: startDate,
+		EndDate:   endDate,
+		RoomID:    roomID,
 	}
 
 	form := forms.New(r.PostForm)
@@ -88,6 +113,11 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 			Form: form,
 			Data: data,
 		})
+	}
+
+	err = m.DB.InsertReservation(reservation)
+	if err != nil {
+		helpers.ServerError(w, err)
 	}
 
 	m.App.Session.Put(r.Context(), "reservation", reservation)
@@ -144,7 +174,7 @@ func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "contact.page.tmpl", &models.TemplateData{})
 }
 
-func (m *Repository) ReserationSummary(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
 	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
 	if !ok {
 		m.App.ErrorLog.Println("can't get error from session")
